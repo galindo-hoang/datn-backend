b6612650a6284742f3f51c071c407f3b2ec95bb4
huy hoang <hmhuy191101@gmail.com>
2023-06-04 03:04:49 +0700

    rm: security

--- src/main/java/com/example/backendservice/common/configure/SecurityConfig.java
+++ src/main/java/com/example/backendservice/common/configure/SecurityConfig.java
@@ -1,20 +1,20 @@
-package com.example.backendservice.common.configure;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.cors().disable();
-        http.authorizeHttpRequests().anyRequest().permitAll();
-        return http.build();
-    }
-}
+//package com.example.backendservice.common.configure;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        http.cors().disable();
+//        http.authorizeHttpRequests().anyRequest().permitAll();
+//        return http.build();
+//    }
+//}
